e3338e2953ce6bcb2058a869e605b89954229ac9
shiv123456324 <shivlad295@gmail.com>
2025-08-06 18:02:24 +0530

    Add files via upload

---
+++ codblock 4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+ int a,b,c;
+ printf("ENTER TWO NUMBERS FOR SUBTRACTION");
+ scanf("%d %d",&a,&b);
+ c=a-b;
+ printf("%d - %d = %d",a,b,c);
+
+}
---
+++ codblock2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter 2 number for the addition");
+    scanf("%d %d",&a,&b);
+    c=a+b;
+    printf("%d + %d = %d\n",a,b,c);
+}
