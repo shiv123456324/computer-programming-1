43aeff7a6e8084ec455551660ff1668c8b82d05c
shiv123456324 <shivlad295@gmail.com>
2025-08-13 17:16:14 +0530

    Add files via upload

---
+++ codblock10.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    float a,b,c;
+    printf("ENTER CURRENCY IN DOLLAR");
+    scanf("%f",&a);
+    b=a*48;
+
+    c=b/70;
+    printf("amount in pound is %f",c);
+}
