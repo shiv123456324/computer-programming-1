8307d3e6287c4a9d7dcf2dbe065748f76cb73849
shiv123456324 <shivlad295@gmail.com>
2025-08-13 16:38:38 +0530

    Add files via upload

---
+++ project 1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    printf("enter a number");
+    int number;
+    scanf("%d",&number);
+    if(number%2==0)
+    {
+        printf("number is even");
+
+    }
+    else
+        {
+            printf("number is odd");
+
+    }
+    return 0;
+}
+
