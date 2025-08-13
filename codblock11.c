3c650c9ab384c7e469386a0e64f1e42a9f0c3952
shiv123456324 <shivlad295@gmail.com>
2025-08-13 17:22:16 +0530

    Add files via upload

---
+++ codblock11.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    float a,b;
+    printf("ENTER amount in grams");
+    scanf("%f",&a);
+    b=a/1000;
+    printf("%f/1000=%f.kg",a,b);
+
+}
+
---
+++ codblock12.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    float a,b;
+    printf("ENTER amount in kg");
+    scanf("%f",&a);
+    b=a*1000;
+    printf("%f/1000=%f.grams",a,b);
+
+}
+
