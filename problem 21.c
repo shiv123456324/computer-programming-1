6d456721e74e3ccb6f8cf74654faf99c58ee91d3
shiv123456324 <shivlad295@gmail.com>
2025-09-10 16:29:32 +0530

    Add files via upload

---
+++ problem 21.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{   int a,n;
+    printf("enter three digit no:\n");
+    scanf("%d",&n);
+    for(int i=0;i<3;i++)
+    {
+        a=n%10;
+        printf("%d",a);
+        n=n/10;
+    }
+    return 0;
+}
